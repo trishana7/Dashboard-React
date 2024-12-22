fdef91fa50e0ffd76efa6631e5f04cc0938fc05e
trishana <trishurai7@gmail.com>
2024-12-22 00:46:41 -0600

    rearrage the code and removed unwanted image

--- api/dashboardApi.ts
+++ api/dashboardApi.ts
@@ -1,6 +1,7 @@
-const ORDER_LIST_API = "/OrderList.json?key=a7f10450";
 import { createApiConfig } from "@/api/apiHelper";
 import type { AxiosRequestConfig } from "axios";
+
+const ORDER_LIST_API = "/OrderList.json?key=a7f10450";
 
 export const getDashboardOrderList = (): AxiosRequestConfig =>
   createApiConfig(ORDER_LIST_API, "get");
